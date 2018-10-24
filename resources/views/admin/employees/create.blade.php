1d3ee9d1ee744313a309f33663d2a50c69146720
joabuono <joaquina@alasit.com>
2018-10-24 15:27:45 -0300

    re #32 creacion de usuarios empleados

---
+++ resources/views/admin/employees/create.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Create User</h1>
+        <hr>
+
+        <form action="{{ route('admin.employees.store') }}" method="post">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <label for="first_name">First Name:</label>
+                <input type="text" name="first_name" id="first_name" class="form-control {{ $errors->has('firts_name') ? ' is-invalid' : '' }}" value="{{ old('first_name') }}" required>
+                @if ($errors->has('first_name'))
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $errors->first('first_name') }}</strong>
+                    </span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <label for="last_name">Last Name:</label>
+                <input type="text" name="last_name" id="last_name" class="form-control {{ $errors->has('last_name') ? ' is-invalid' : '' }}" value="{{ old('last_name') }}" required>
+                @if ($errors->has('last_name'))
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $errors->first('last_name') }}</strong>
+                    </span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="text" name="email" id="email" class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" value="{{ old('email') }}" required>
+                @if ($errors->has('email'))
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <label for="github">Github:</label>
+                <input type="text" name="github" id="github" class="form-control" value="{{ old('github') }}">
+            </div>
+
+            <div class="form-group">
+                <label for="phone">Phone:</label>
+                <input type="text" name="phone" id="phone" class="form-control" value="{{ old('phone') }}">
+                @if ($errors->has('phone'))
+                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('phone') }}</strong>
+                                    </span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <button class="btn btn-primary"><i class="fa fa-plus-circle"></i> Create</button>
+            </div>
+        </form>
+
+        <a class="btn btn-link" href="{{ route('admin.employees.index') }}"><i class="fa fa-arrow-circle-left"></i> Back</a>
+
+    </div>
+@endsection
--- resources/views/admin/employees/index.blade.php
+++ resources/views/admin/employees/index.blade.php
@@ -1,49 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="content">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header card-title text-center">Employees</div>
+    <div class="container">
+        <h1>EMPLOYEES</h1>
+        <hr>
+        <a class="btn btn-primary" href="{{ route('admin.employees.create')}}"><i class="fa fa-plus"></i> Create new User</a>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        <table class="table table-striped table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>Email</th>
-                                    <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>Estate</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($users as $user)
-                                <tr>
-                                    <td>{{ $user->email }}</td>
-                                    <td>{{ $user->profile ? $user->profile->first_name : "-" }}</td>
-                                    <td>{{ $user->profile ? $user->profile->last_name : "-" }}</td>
-                                    <td>
-                                        @if ($user->trashed())
-                                            {{ __('Disabled') }}
-                                        @else
-                                            {{ __('Enabled') }}
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <a class="btn btn-success" href="{{ route('admin.employees.show', $user->id) }}">View</a>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
             </div>
-        </div>
+        @endif
+
+        <br>
+        <br>
+
+        <table class="table table-striped table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Email</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Estate</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->profile ? $user->profile->first_name : "-" }}</td>
+                    <td>{{ $user->profile ? $user->profile->last_name : "-" }}</td>
+                    <td>
+                        @if ($user->trashed())
+                            {{ __('Disabled') }}
+                        @else
+                            {{ __('Enabled') }}
+                        @endif
+                    </td>
+                    <td>
+                        <a class="btn btn-success" href="{{ route('admin.employees.show', $user->id) }}">View</a>
+            @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
