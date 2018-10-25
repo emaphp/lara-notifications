d3890eb50776992dfbeb28e133de35a4ef6b8634
julietagatica <julieta@alasit.com>
2018-10-25 09:56:14 -0300

    re #8 Modificaciones

--- resources/views/admin/employees/index.blade.php
+++ resources/views/admin/employees/index.blade.php
@@ -1,49 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="content">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header card-title text-center">Employees</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        <table class="table table-striped table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>Email</th>
-                                    <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>Estate</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($users as $user)
-                                <tr>
-                                    <td>{{ $user->email }}</td>
-                                    <td>{{ $user->profile ? $user->profile->first_name : "-" }}</td>
-                                    <td>{{ $user->profile ? $user->profile->last_name : "-" }}</td>
-                                    <td>
-                                        @if ($user->trashed())
-                                            {{ __('Disabled') }}
-                                        @else
-                                            {{ __('Enabled') }}
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <a class="btn btn-success" href="{{ route('admin.employees.show', $user->id) }}">View</a>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+    <div class="container" style="margin-top:45px;">
+        <h1>EMPLOYEES</h1>
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
             </div>
-        </div>
+        @endif
+
+        <table class="table">
+            <thead class="thead-dark">
+            <tr>
+                <th>Email</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Estate</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->profile ? $user->profile->first_name : "-" }}</td>
+                    <td>{{ $user->profile ? $user->profile->last_name : "-" }}</td>
+                    <td>
+                        @if ($user->trashed())
+                            {{ __('Disabled') }}
+                        @else
+                            {{ __('Enabled') }}
+                        @endif
+                    </td>
+                    <td>
+                        <a class="btn btn-primary" href="{{ route('admin.employees.show', $user->id) }}"><i class="fa  fa-eye"></i> Show</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
