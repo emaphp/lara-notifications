ec27ba97c6ec1c62f90a0814a751f302924cf8fe
julietagatica <julieta@alasit.com>
2018-11-16 09:52:27 -0300

    re #87 Mostrar cumpleaños x mes

--- app/Alas/Commands/CumpleCommand.php
+++ app/Alas/Commands/CumpleCommand.php
@@ -10,35 +10,55 @@ class CumpleCommand implements CommandInterface
 {
 
     private $birthdays;
+    private $month;
 
-    public function __construct()
+    public function __construct($month = null)
     {
+        $this->month = $month;
         $this->birthdays = array();
     }
 
     public function execute()
     {
         setlocale(LC_TIME, 'es_AR.UTF-8');
-        $currentDate = Carbon::now();
-
-        $startWeek = $currentDate->startOfWeek(Carbon::MONDAY)->copy();
-        $endWeek = $currentDate->endOfWeek(Carbon::SUNDAY)->copy();
 
         $employees = User::where('type','employee')->get();
+
+        $currentDate = Carbon::now();
+        $year = $currentDate->year;
+
+        if (is_null($this->month)) {
+            $startDate = $currentDate->startOfWeek(Carbon::MONDAY)->copy();
+            $endDate = $currentDate->endOfWeek(Carbon::SUNDAY)->copy();
+        }
+        else {
+            $month = $currentDate->month;
+            if ($this->month <= $month) {
+                $year++;
+            }
+            $date = Carbon::createFromDate($year, $this->month, 1);
+            $startDate = $date->startOfMonth()->copy();
+            $endDate = $date->endOfMonth()->copy();
+        }
 
         foreach ($employees as $employee) {
             $profile = $employee->profile()->first();
             if (!is_null($profile) && !is_null($profile->birthdate)) {
                 $birthdate = Carbon::parse($profile->birthdate);
-                $birthdate->year = $currentDate->year;
-                if ($birthdate->greaterThanOrEqualTo($startWeek) && $birthdate->lessThanOrEqualTo($endWeek)) {
+                $birthdate->year = $year;
+                if ($birthdate->greaterThanOrEqualTo($startDate) && $birthdate->lessThanOrEqualTo($endDate)) {
                     $this->birthdays[$employee->id] = $employee->name . ' (' . ucfirst($birthdate->formatLocalized('%A')) . ' ' . $birthdate->day . ').';
                 }
             }
         }
 
         if ($this->birthdays == []) {
-            return 'No hay cumpleaños esta semana';
+            if (is_null($this->month)) {
+                return 'No hay cumpleaños esta semana';
+            }
+            else {
+                return 'No hay cumpleaños este mes';
+            }
         }
         else {
             return $this->birthdays;
